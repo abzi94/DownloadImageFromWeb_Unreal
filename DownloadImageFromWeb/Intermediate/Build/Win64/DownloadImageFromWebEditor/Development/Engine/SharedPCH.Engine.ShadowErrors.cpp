1a8e98fa901a6684057e6ed978921a5ceb663a9c
Abraiz Khan <abraizkhan94@gmail.com>
2021-04-22 00:29:01 +0400

    removed unused folders

--- DownloadImageFromWeb/Intermediate/Build/Win64/DownloadImageFromWebEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++
@@ -1,1 +0,0 @@
-#include "C:/Users/abrai/Documents/Unreal Projects/DownloadImageFromWeb/Intermediate/Build/Win64/DownloadImageFromWebEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
--- DownloadImageFromWeb/Intermediate/Build/Win64/DownloadImageFromWebEditor/Development/UnrealEd/SharedPCH.UnrealEd.ShadowErrors.cpp
+++
@@ -1,1 +0,0 @@
-#include "C:/Users/abrai/Documents/Unreal Projects/DownloadImageFromWeb/Intermediate/Build/Win64/DownloadImageFromWebEditor/Development/UnrealEd/SharedPCH.UnrealEd.ShadowErrors.h"
